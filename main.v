4b5514faaae2f3616dfae5ada3a6289edf66412a
k0ntinuum <k0ntinuum@proton.me>
2023-10-03 05:32:34 -0400

    a

--- main.v
+++ main.v
@@ -1,8 +1,6 @@
 fn main() {
-	// k := key(2,7)
-	cls()
-	move_cursor(0,10)
-	mut m := square_machine(2, 6)
-	m.test_encode(32)
-	//m.test_encrypt(64 , 40)
+	mut m := square_machine(2,5)
+	m.test_encode(20)
 }
+
+
